f5e10f909780d051df0323babcfd22348e9216a7
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-21 18:28:18 +0200

    implementazione carosello

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,24 +5,67 @@
  * usiamo la direttiva v-for per stampare tutti i thumbnails.
 Al termine di questa fase ci ritroveremo con lo stesso slider, 
 ma costruito dinamicamente attraverso Vue-js.
+
+--3 millestone
+Ora rendiamo lo slider dimanico. Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi: - titolo e - testo.
+Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre
  */
 
 const app = new Vue(
     {
         el : "#app",
         data:{
-            place: {
-                images:[
-                    "./assets/img/01.jpg",
-                    "./assets/img/02.jpg",
-                    "./assets/img/03.jpg",
-                    "./assets/img/04.jpg",
-                    "./assets/img/05.jpg"
-                ]
-            }
+            
+            active_images:0,
+            //struttura dati
+            place: [
+                {
+                    image: './assets/img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: './assets/img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: './assets/img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: './assets/img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: './assets/img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                }
+            ]
+
+            
+               
+                
+            
         },
         methods:{
-
+            prew(){
+                console.log(this.prew);
+                this.active_images--
+                if(this.active_images == 0){
+                    console.log("siamo in svezia");
+                }
+                
+            },
+            next(){
+                console.log(this.next)
+                this.active_images++
+            }
+            
         }
     }
 )
+
